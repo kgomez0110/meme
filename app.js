db4670dee5a64cc1db6da76cda00b12cb9d33ee8
VictorAung <VictorAung2021@u.northwestern.edu>
2019-02-17 02:37:43 -0600

    fixing app for online deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 const port = 3000
 
