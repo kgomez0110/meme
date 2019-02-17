85463ceaa02562a2e71a7977fa74995739764305
Kenneth Gomez <kenneth@Kenneths-MacBook-Pro-2.local>
2019-02-16 20:13:48 -0600

    uses go button

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 var z = 0;
+var started = false;
 function drawImage(event) {
-  var old_html = document.getElementById("screen").innerHTML;
-  if (old_html.includes("Select audio")){
-    old_html = "";
+  if (started){
+    var old_html = document.getElementById("screen").innerHTML;
+    if (old_html.includes("Select audio")){
+      old_html = "";
+    }
+    console.log(old_html);
+    var x = event.clientX;
+    var y = event.clientY;
+    console.log(x);
+    console.log(y);
+    document.getElementById("screen").innerHTML = old_html + '\n <img src="./red.jpg" style="position: absolute; left:' + x + 'px; top:' + y + 'px; z-index: ' + z + ';">'; 
+    z++;
+    console.log(z);
   }
-  console.log(old_html);
-  var x = event.clientX;
-  var y = event.clientY;
-  console.log(x);
-  console.log(y);
-  document.getElementById("screen").innerHTML = old_html + '\n <img src="./red.jpg" style="position: absolute; left:' + x + 'px; top:' + y + 'px; z-index: ' + z + ';">'; 
-  z++;
-  console.log(z);
+}
+
+function start(){
+  document.getElementById("screen").style.z_index = 0;
+  document.getElementById("menu").style.z_index = -1;
+  document.getElementById("menu").innerHTML = "";
+  document.getElementById("menu").className = "";
+  started = true;
 }
