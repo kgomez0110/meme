b711ba0b70a8d88afa851c14e41ad7aaf4f192f5
VictorAung <VictorAung2021@u.northwestern.edu>
2019-02-17 02:41:50 -0600

    fixing app for deployment

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
