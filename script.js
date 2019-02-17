41692eb4d5370696ed148f0f00874b2203cd7432
Kenneth Gomez <kenneth@dhcp-10-105-224-63.wireless.northwestern.private>
2019-02-16 22:08:34 -0600

    url

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var z = 0;
 var started = false;
+var imgUrl = "";
+var audioUrl = "";
 function drawImage(event) {
   if (started){
     var old_html = document.getElementById("screen").innerHTML;
@@ -9,10 +11,7 @@ function drawImage(event) {
     console.log(old_html);
     var x = event.clientX;
     var y = event.clientY;
-    var uploadedFile = document.getElementById("myFile");
-    console.log(x);
-    console.log(y);
-    document.getElementById("screen").innerHTML = old_html + '\n <img src=' + string(uploadedFile) + 'style="position: absolute; left:' + x + 'px; top:' + y + 'px; z-index: ' + z + ';">';
+    document.getElementById("screen").innerHTML = old_html + '\n <img src="' + imgUrl + '" style="position: absolute; left:' + x + 'px; top:' + y + 'px; z-index: ' + z + ';">';
     z++;
     console.log(z);
   }
@@ -21,6 +20,8 @@ function drawImage(event) {
 function start(){
   document.getElementById("screen").style.z_index = 0;
   document.getElementById("menu").style.z_index = -1;
+  imgUrl = document.getElementById("myFile").value;
+  audioUrl = document.getElementById("myAudio").value;
   document.getElementById("menu").innerHTML = "";
   document.getElementById("menu").className = "";
   started = true;
