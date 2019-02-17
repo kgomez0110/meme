f591321d7ac725bbf5723f61eca68bc0e8f7a941
VictorAung <VictorAung2021@u.northwestern.edu>
2019-02-17 01:55:29 -0600

    finished ALL the audio

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 var z = 0;
 var started = false;
 var imgUrl = "";
-var audioUrl = "";
-var bgUrl = "";
+var audioSelection = "";
+var audioFile = new Audio();
+//var bgUrl = "";
 function drawImage(event) {
   if (started){
     var old_html = document.getElementById("screen").innerHTML;
@@ -12,9 +13,12 @@ function drawImage(event) {
     console.log(old_html);
     var x = event.clientX;
     var y = event.clientY;
-    document.getElementById("screen").innerHTML = old_html + '\n <img src="' + imgUrl + '" style="position: absolute; left:' + x + 'px; top:' + y + 'px; z-index: ' + z + ';">';
+    document.getElementById("screen").innerHTML = old_html + '\n <img src="' + imgUrl + '" style="position: absolute; left:' + x + 'px; top:' + y + 'px; z-index: ' + z + '; pointer-events: none; user-drag: none; user-select: none;">';
     z++;
-    console.log(z);
+    const newAudio = audioFile.cloneNode();
+    newAudio.play();
+    console.log(audioSelection);
+
   }
 }
 
@@ -22,7 +26,8 @@ function start(){
   document.getElementById("screen").style.z_index = 0;
   document.getElementById("menu").style.z_index = -1;
   imgUrl = document.getElementById("myFile").value;
-  audioUrl = document.getElementById("myAudio").value;
+  audioSelection = document.getElementById("audioSelector").value;
+  audioFile = new Audio("./assets/" + audioSelection);
   bgUrl = document.getElementById("myBackground").value;
   document.getElementById("menu").innerHTML = "";
   document.getElementById("menu").className = "";
