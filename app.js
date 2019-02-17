d5e69be44230b3f0487802bce8f45d2f98613698
VictorAung <VictorAung2021@u.northwestern.edu>
2019-02-17 02:57:15 -0600

    changing to port 8080

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-const port = 3000
+const port = 8080
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
